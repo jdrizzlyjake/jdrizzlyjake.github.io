ec656538bca9f0a5b9341515fb2d45b602d0742a
jdrizzlyjake <160051762+jdrizzlyjake@users.noreply.github.com>
2024-03-18 16:47:25 +0000

    did stuff

--- home.js
+++ home.js
@@ -3,3 +3,19 @@ function showAlert()
     {
         alert("This is a test alert!");
     }
+console.log(81+55+124+4+13+87);
+
+console.log((235+3)/41);
+
+let a = 10;
+console.log(a); // Should print 10
+console.log(9 * a); // Multiplies `a` by 9
+let b = 7 * a;
+console.log(b); // Prints the result of `b`
+let c = 3.4 * b;
+console.log(c);
+
+const MAX = 10;
+let actual = 100;
+let percentage = actual / MAX;
+console.log(percentage);
